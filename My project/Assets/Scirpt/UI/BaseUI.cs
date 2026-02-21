525ea283df0e61f0821376f9651e650a9e3a2522
PKingTeak <119428177+PKingTeak@users.noreply.github.com>
2026-02-21 21:02:26 +0900

    UI작업중 GameManager 분리 진행중

---
+++ My project/Assets/Scirpt/UI/BaseUI.cs
@@ -0,0 +1,80 @@
+using Unity.VisualScripting;
+using UnityEditor.Analytics;
+using UnityEngine;
+
+public class BaseUI : MonoBehaviour
+{
+    protected CanvasGroup canvasGroup;
+    public bool isActive { get; private set; }
+
+    protected bool _isInitialized = false;
+    
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    protected void Init()
+    {
+        if (_isInitialized)
+        {
+            return;
+        }
+
+        OnInitilze();
+        _isInitialized = true;
+        
+    }
+
+    protected virtual void Hide()
+    {
+        if (!_isInitialized)
+        {
+            Init();
+        }
+
+        SetCanvsState(_alpha: 0f, _interactable: false, _blocksRaycasts: false);
+
+        isActive = false;
+        OnHide();
+        _isInitialized = false;
+
+    }
+
+    public void Interactable(bool value)
+    {
+        if (canvasGroup == null)
+        {
+            return;
+        }
+
+        canvasGroup.interactable = value;
+        canvasGroup.blocksRaycasts = value;
+    }
+
+    protected void Show()
+    {
+        if (!_isInitialized)
+        {
+            Init();
+        }
+        this.gameObject.SetActive(true);
+
+        
+    }
+
+    protected void SetCanvsState(float _alpha, bool _interactable, bool _blocksRaycasts)
+    {
+        if (canvasGroup == null)
+        {
+            return;
+        }
+
+        canvasGroup.alpha = _alpha;
+        canvasGroup.interactable = _interactable;
+        canvasGroup.blocksRaycasts = _blocksRaycasts;
+    }
+
+    protected virtual void OnInitilze() { } //초기화 해야 하는것들 켤때 말고 
+    protected virtual void OnShow() { } //보여줄때 초기화 할것들
+    protected virtual void OnHide() { } //소멸할때 해제해야 하는것들 
+}
